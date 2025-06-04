f26dfa40e85912ee200bd7ab0f572a30309276cb
강동욱 <thery91@naver.com>
2025-06-04 18:38:26 +0900

    ✨ 종료 기능 추가
    반복문 추가 및 종료 기능 추가

--- 프로젝트/12_atm/atm.py
+++ 프로젝트/12_atm/atm.py
@@ -1,1 +1,14 @@
+#잔액 초기화
 balance = 1000
+
+mode = input("기능을 선택해주세요: (1. 입금, 2. 출금, 3.영수증 보기, 4. 종료): ")
+
+if mode == "종료":
+    print("종료 되었습니다.")
+
+if mode == "입금"
+    
+    deposit = int(input("입금 할 금액을 입력해주세요."))
+
+    balance_deposit = balane+deposit
+    print("잔고금액 : {}", balance_deposit)
