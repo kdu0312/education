a817c4a334ded6b751de81d38b7874a06835ed18
강동욱 <thery91@naver.com>
2025-06-05 09:28:11 +0900

    Update atm.py

--- 프로젝트/12_atm/atm.py
+++ 프로젝트/12_atm/atm.py
@@ -1,14 +1,36 @@
 #잔액 초기화
 balance = 1000
+x=0
 
-mode = input("기능을 선택해주세요: (1. 입금, 2. 출금, 3.영수증 보기, 4. 종료): ")
+while True:
 
-if mode == "종료":
-    print("종료 되었습니다.")
+    #모드 선택창
+    mode = input("기능을 선택해주세요: (1.입금, 2.출금, 3.영수증 보기, 4.종료): ") ## 참거짓은 힘들것 같고 if로 조져야 할것같은데데
 
-if mode == "입금"
+    #종료 메세지
+    if mode == "4":
+        print("종료 되었습니다.")
+        break
+
+    #입금 메세지
+    if mode == "1":
     
-    deposit = int(input("입금 할 금액을 입력해주세요."))
+        deposit = int(input("입금 할 금액을 입력해주세요: "))
 
-    balance_deposit = balane+deposit
-    print("잔고금액 : {}", balance_deposit)
+        balance += deposit
+        print("잔고금액 : ", balance)
+
+        return balance
+
+    #출금 메세지
+    if mode == "2":
+    
+        deposit = int(input("출금금 할 금액을 입력해주세요: "))
+    
+        if deposit <= deposit:
+            balance -= deposit
+            print("잔고금액 : ", balance) 
+    
+        else:
+            print("잔액이 부족합니다.")
+
